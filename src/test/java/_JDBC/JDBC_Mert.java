b196f5ad5b8c95e27deceaa7a9a9f5edaf9f2100
MertOzmen <avmertozmen93@gmail.com>
2024-05-02 00:55:51 +0300

    First query has been completed.

--- src/test/java/_JDBC/JDBC_Mert.java
+++ src/test/java/_JDBC/JDBC_Mert.java
@@ -1,17 +1,73 @@
 package _JDBC;
 
+
 import org.testng.annotations.Test;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class JDBC_Mert extends JDBCParent{
+import static org.testng.Assert.assertFalse;
+
+public class JDBC_Mert extends JDBCParent {
 
     @Test
-    public void Test1(){
+    public void Test1() {
+
+      /*
+      -- SORGU 3 :
+
+      -- Calculate the average salary of all employees.
+      -- Tüm çalışanların ortalama maaşını hesapla.
+
+       */
+
+        String sorgu =
+                "select avg(salary) as CalisanOrtalamaMaas from salaries;";
+
+        try {
+            DBConnectionOpen();
+            List<List<String>> donenTablo = getListData(sorgu);
+            assertFalse(donenTablo.isEmpty(), "Sorgu sonucu boş olmamalıdır.");
+            for (List<String> satir : donenTablo) {
+                for (String kolon : satir)
+                    System.out.print(kolon + "\t");
+                System.out.println();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            DBConnectionClose();
+        }
 
     }
 
+    @Test
+    public void Test2(){
 
+    }
+
+    public List<List<String>> getListData(String sorgu) throws SQLException {
+        List<List<String>> tablo = new ArrayList<>();
+
+        ResultSet rs = sorguEkrani.executeQuery(sorgu);
+        ResultSetMetaData rsmd = rs.getMetaData();
+
+        ArrayList<String> kolonSatiri = new ArrayList<>();
+        for (int i = 1; i <= rsmd.getColumnCount(); i++)
+            kolonSatiri.add(rsmd.getColumnName(i));
+        tablo.add(kolonSatiri);
+
+        int satirSayisi = 0; // İlk 10 satırı saymak için bir sayaç tanımlıyoruz
+        while (rs.next() && satirSayisi < 20) { // İlk 20 satırı almak için döngüyü ayarlıyoruz
+            ArrayList<String> satir = new ArrayList<>();
+            for (int i = 1; i <= rsmd.getColumnCount(); i++)
+                satir.add(rs.getString(i));
+            tablo.add(satir);
+            satirSayisi++; // Her döngüde sayaçı arttırıyoruz
+        }
+        return tablo;
+    }
 
 
 }
