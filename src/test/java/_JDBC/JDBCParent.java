417b8bfa8635bc943dab2176b560c88783d17ae6
elcnburak <elcnburak@hotmail.com>
2024-05-04 12:13:26 +0100

    JDBCParent getListData added

--- src/test/java/_JDBC/JDBCParent.java
+++ src/test/java/_JDBC/JDBCParent.java
@@ -1,36 +1,69 @@
 package _JDBC;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JDBCParent {
+
     public static Connection baglanti;
     public static Statement sorguEkrani;
 
-    public static void DBConnectionOpen()
-    {
+    // Veritabanına bağlantıyı açan metod
+    public static void DBConnectionOpen() {
         String url = "jdbc:mysql://db-technostudy.ckr1jisflxpv.us-east-1.rds.amazonaws.com/employees";
-        String username = "root";
-        String password = "'\"-LhCB'.%k[4S]z";
+        String kullaniciAdi = "root";
+        String sifre = "'\"-LhCB'.%k[4S]z";
 
         try {
-            baglanti = DriverManager.getConnection(url, username, password);
+            baglanti = DriverManager.getConnection(url, kullaniciAdi, sifre);
             sorguEkrani = baglanti.createStatement();
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.println("ex.getMessage() = " + ex.getMessage());
         }
     }
 
-    public static void DBConnectionClose()
-    {
+    // Veritabanı bağlantısını kapatan metod
+    public static void DBConnectionClose() {
         try {
             baglanti.close();
         } catch (SQLException e) {
             System.out.println("ex.getMessage() = " + e.getMessage());
         }
+    }
+
+    public static List<List<String>> getListData(String sorgu) throws SQLException {
+        List<List<String>> tablo = new ArrayList<>();
+
+        try {
+            DBConnectionOpen();
+            ResultSet rs = sorguEkrani.executeQuery(sorgu);
+            ResultSetMetaData rsmd = rs.getMetaData(); // sütun sayısı
+
+            // Sütun isimlerini tablonun ilk satırına ekler
+            ArrayList<String> kolonSatiri = new ArrayList<>();
+            for (int i = 1; i <= rsmd.getColumnCount(); i++)
+                kolonSatiri.add(rsmd.getColumnName(i));
+            tablo.add(kolonSatiri);
+
+            while (rs.next()) {
+
+                ArrayList<String> satir = new ArrayList<>();
+                // Bu satırdaki elemanları satır listesine ekler
+                // rs.getString(1); // 1. sütun
+                // rs.getString(2); // 2. sütun
+                for (int i = 1; i <= rsmd.getColumnCount(); i++)
+                    satir.add(rs.getString(i));
+
+                // Satıra ekleme bittiğinde tabloya ekler
+                tablo.add(satir);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            DBConnectionClose();  // Hata olsa da olmasa da bağlantıyı kapatır
+        }
+
+        return tablo;
     }
 }
